51bf4ac58c566f312db3b1470a7c0b0a4fb89917
Tony Nelson <tonynelson19@gmail.com>
2014-03-04 10:13:42 -0600

    Formatting updates inside environment detection
    Pretty random and trivial, but does this improve the readability of the page over using if/else statements?

--- web/index.php
+++ web/index.php
@@ -12,17 +12,23 @@
  */
 
 // Define application environment
-defined('APPLICATION_ENV')
+defined('APPLICATION_ENV') 
     || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'prod'));
 
-if (APPLICATION_ENV === 'prod') {
-    require dirname(__FILE__) . '/app.php';
-} elseif (
-       APPLICATION_ENV === 'dev'
-    || APPLICATION_ENV === 'staging'
-) {
-    require dirname(__FILE__) . '/app_dev.php';
-} else {
-    header('HTTP/1.0 500 Internal Server Error');
-    exit('Improper APPLICATION_ENV set. See ' . basename(__FILE__) . ' for more information.');
+switch (APPLICATION_ENV) {
+
+    case 'prod':
+        require __DIR__ . '/app.php';
+        break;
+
+    case 'dev':
+    case 'staging':
+    case 'test':
+        require __DIR__ . '/app_dev.php';
+        break;
+
+    default:
+        header('HTTP/1.0 500 Internal Server Error');
+        exit('Improper APPLICATION_ENV set. See ' . basename(__FILE__) . ' for more information.');
+
 }
